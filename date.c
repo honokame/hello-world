5d6c73282811a7c84eebbeb4bad301007486196e
okuguchihonoka <e1607@s.akashi.ac.jp>
2017-04-29 11:51:58 +0900

    add new file

---
+++ date.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+
+{
+	int m =4,d = 25;
+	printf("今日は %d 月 %d 日です\n",m,d);
+
+	return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>//標準入出力関数が使える
+
+int main(int argc,char**argv)
+{
+	//このように行空白の行部分に命令を並べる（ほかは毎回同じ）
+	return (0);
+}
