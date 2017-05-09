f5bb07eae0acf7ce68de7d43f41131b32b8a609a
okuguchihonoka <e1607@s.akashi.ac.jp>
2017-05-09 10:21:20 +0900

    add new file

---
+++ break.c
@@ -0,0 +1,17 @@
+// エラー１ 文字列  
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc,char**argv)
+{
+	char s[100];
+	strcpy(s, "123456789");
+	printf("%s\n",s);
+	s[3] = '\0';
+	printf("%s\n",s);
+
+	return 0;
+}
+
+
+
---
+++ break2.c
@@ -0,0 +1,12 @@
+// エラー２ 文字列  
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc,char**argv)
+{
+	char s[100];
+	strcpy(s,"今日は雨が降ります");
+	printf("%s\n",s);
+	printf("%c\n",s[4]);
+	return 0;
+}
