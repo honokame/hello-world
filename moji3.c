94b9d1b386f33edc7a0906a2c980146e0bfb0bef
okuguchihonoka <e1607@s.akashi.ac.jp>
2017-07-30 17:08:55 +0900

    Update moji3.c

--- moji3.c
+++ moji3.c
@@ -5,29 +5,30 @@
 
 int main(int argc,char**argv)
 {
-	int i,j;
+        int i,j;
 	char name[9][9],change[9];
 
 	printf("名前を入力してください\n");
 	for(i = 0;i < 10;i++){
-		printf("%d人目:",i+1);
-		scanf("%s",name[i]);
-		}
+	    printf("%d人目:",i+1);
+	    scanf("%s",name[i]);
+	}
 
 	for(i = 1;i < 10;i++){
-		for(j = 1;j < 10;j++){
-		if(strcmp(name[j-1],name[j]) == 0)
-			continue;	
-		else if(strcmp(name[j-1],name[j]) > 0){
-				strcpy(change,name[j-1]);
-				strcpy(name[j-1],name[j]);
-				strcpy(name[j],change);
-				}
-			}
+          for(j = 1;j < 10;j++){
+	    if(strcmp(name[j-1],name[j]) == 0)
+		continue;	
+	   
+	    else if(strcmp(name[j-1],name[j]) > 0){
+		strcpy(change,name[j-1]);
+		strcpy(name[j-1],name[j]);
+		strcpy(name[j],change);
+	    }
+	  }
 	}
 	printf("\n名前を並び替えます\n");
 	for(i = 0;i < 10;i++)
-		printf("%s\n",name[i]);
+	    printf("%s\n",name[i]);
 
 return 0;
 }
